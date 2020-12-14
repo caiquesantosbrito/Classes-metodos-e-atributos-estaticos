026e9850fadf2cec9ce6cc049cfea3a67c1eecc5
Caique Santos <caisbrito2003@gmail.com>
2020-12-14 20:24:40 -0300

    Calculadora de conversão feita

---
+++ Calculadora de Moeda/Classe/Conversor.cs
@@ -0,0 +1,21 @@
+namespace Calculadora_de_Moeda.Classe
+{
+    public static class Conversor
+    {
+        private static float cotacaoDolar = 5.10f;
+        private static float cotacaoEuro = 6.22f; 
+        public static float valorUsuario;
+        public static float ConverterDolarParaReal(){
+            return valorUsuario * cotacaoDolar;
+        }
+        public static float ConverterRealParaDolar(){
+            return valorUsuario / cotacaoDolar;
+        }
+        public static float CoverterEuroParaReal(){
+            return valorUsuario * cotacaoEuro;
+        }
+        public static float ConverterRealParaEuro(){
+            return valorUsuario / cotacaoEuro;
+        }
+    }
+}
---
+++ Calculadora de Moeda/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Calculadora_de_Moeda.Classe;
+
+namespace Calculadora_de_Moeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Pow(2,5));
+
+            Console.WriteLine("Digite um valor:");
+            Conversor.valorUsuario = float.Parse(Console.ReadLine());
+            Console.WriteLine("Valor de dolar para real é: ");
+            Console.WriteLine(Conversor.ConverterDolarParaReal());
+            Console.WriteLine("Valor de real para dolar é: ");
+            Console.WriteLine(Conversor.ConverterRealParaDolar());
+            Console.WriteLine("Valor de euro para para real é: ");
+            Console.WriteLine(Conversor.CoverterEuroParaReal());
+            Console.WriteLine("Valor de real para euro é: ");
+            Console.WriteLine(Conversor.ConverterRealParaEuro());
+
+            Testar();
+        }
+        static void Testar(){
+            
+        }
+    }
+}
